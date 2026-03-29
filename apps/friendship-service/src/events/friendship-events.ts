4c9565bafce1c88e38534addb9407b9598f61833
hanhpham204 <hp926405@gmail.com>
2026-03-29 18:44:05 +0700

    perf(friendship): lazy-load heavy module

---
+++ apps/friendship-service/src/events/friendship-events.ts
@@ -0,0 +1,65 @@
+/**
+ * Friendship event types and interfaces for Kafka
+ * These events are consumed by other services (ChatCore, NotificationService, etc.)
+ *
+ * Event types are defined in @app/kafka KAFKA_TOPICS.FRIENDSHIP
+ */
+
+import { KAFKA_TOPICS } from '@app/kafka';
+
+export interface BaseFriendshipEvent {
+  eventId: string;
+  type: string;
+  timestamp: string;
+}
+
+export interface FriendRequestSentEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.REQUEST_SENT;
+  fromUserId: string;
+  toUserId: string;
+}
+
+export interface FriendRequestAcceptedEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.REQUEST_ACCEPTED;
+  userA: string;
+  userB: string;
+}
+
+export interface FriendRequestRejectedEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.REQUEST_REJECTED;
+  userA: string;
+  userB: string;
+}
+
+export interface FriendRequestCanceledEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.REQUEST_CANCELED;
+  canceledBy: string;
+  targetUserId: string;
+}
+
+export interface FriendRemovedEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.REMOVED;
+  userA: string;
+  userB: string;
+}
+
+export interface UserBlockedEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.BLOCKED;
+  blocker: string;
+  blocked: string;
+}
+
+export interface UserUnblockedEvent extends BaseFriendshipEvent {
+  type: typeof KAFKA_TOPICS.FRIENDSHIP.UNBLOCKED;
+  unblocker: string;
+  unblocked: string;
+}
+
+export type FriendshipEvent =
+  | FriendRequestSentEvent
+  | FriendRequestAcceptedEvent
+  | FriendRequestRejectedEvent
+  | FriendRequestCanceledEvent
+  | FriendRemovedEvent
+  | UserBlockedEvent
+  | UserUnblockedEvent;
