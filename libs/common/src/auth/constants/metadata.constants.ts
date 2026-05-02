da51321fb24a08e80618c14ce8ef438be9fbcb8d
nguyenhung204 <nguyenhung2004200@gmail.com>
2026-05-02 23:24:44 +0700

    feat(infra): introduce config loader

---
+++ libs/common/src/auth/constants/metadata.constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Metadata keys for decorators
+ */
+export const IS_PUBLIC_KEY = 'isPublic';
+export const ROLES_KEY = 'roles';
+
+/** Injection token for the shared Redis client used to cache JWKS signing keys */
+export const JWKS_REDIS_CLIENT = 'JWKS_REDIS_CLIENT';
